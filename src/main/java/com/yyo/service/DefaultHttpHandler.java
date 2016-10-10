f53e2cacccfbd9e565a6c78f7bd39fb315035054
Cleive Paqui <cpaqui@gmail.com>
2016-10-10 16:23:27 -0300

    Fix angular/html5 issues in heroku.

--- src/main/java/com/yyo/service/DefaultHttpHandler.java
+++ src/main/java/com/yyo/service/DefaultHttpHandler.java
@@ -1,46 +1,27 @@
 package com.yyo.service;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Scanner;
+
+import com.yyo.config.AngularJsHttpHandler;
 
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
-import io.undertow.server.handlers.resource.ClassPathResourceManager;
-import io.undertow.server.handlers.resource.ResourceManager;
-import io.undertow.util.Headers;
 
 public class DefaultHttpHandler implements HttpHandler {
 
-	private final String indexHtml;
+	private static HttpHandler defaultHandler;
 
-	public DefaultHttpHandler() throws IOException {
-		try (ResourceManager staticResources =
-	            new ClassPathResourceManager(this.getClass().getClassLoader(), "dist");) {
-
-			indexHtml = getFile(staticResources.getResource("index.html").getFile());
+	static {
+		try {
+			defaultHandler = AngularJsHttpHandler.create(DefaultHttpHandler.class);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-	}
-
-	private static String getFile(File file) throws FileNotFoundException {
-		StringBuilder result = new StringBuilder("");
-
-		try (Scanner scanner = new Scanner(file)) {
-
-			while (scanner.hasNextLine()) {
-				String line = scanner.nextLine();
-				result.append(line).append("\n");
-			}
-			scanner.close();
-		}
-
-		return result.toString();
 	}
 
 	@Override
 	public void handleRequest(HttpServerExchange exchange) throws Exception {
-		exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html; charset=UTF-8");
-		exchange.getResponseSender().send(indexHtml);
+		exchange.setRelativePath("/");
+		exchange.dispatch(defaultHandler);
 	}
 }
---
+++ src/main/java/com/yyo/service/DefaultResourceHttpHandler.java
@@ -0,0 +1,45 @@
+package com.yyo.service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.server.handlers.resource.ClassPathResourceManager;
+import io.undertow.server.handlers.resource.ResourceManager;
+import io.undertow.util.Headers;
+
+public class DefaultResourceHttpHandler implements HttpHandler {
+
+	private final String indexHtml;
+
+	public DefaultResourceHttpHandler() throws IOException {
+		try (ResourceManager staticResources =
+	            new ClassPathResourceManager(this.getClass().getClassLoader(), "dist");) {
+
+			indexHtml = getFile(staticResources.getResource("index.html").getFile());
+		}
+	}
+
+	private static String getFile(File file) throws FileNotFoundException {
+		StringBuilder result = new StringBuilder("");
+
+		try (Scanner scanner = new Scanner(file)) {
+
+			while (scanner.hasNextLine()) {
+				result.append(scanner.nextLine());
+			}
+			scanner.close();
+		}
+
+		return result.toString();
+	}
+
+	@Override
+	public void handleRequest(HttpServerExchange exchange) throws Exception {
+		exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html; charset=UTF-8");
+		exchange.getResponseSender().send(indexHtml);
+	}
+}
